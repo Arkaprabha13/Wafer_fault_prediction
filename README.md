adc0b827b4872d15cd3a13a205d63a563b43af98
Arkaprabha Banerjee <arkaofficial13@gmail.com>
2024-06-10 21:41:55 +0530

    project setup done

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Wafer_fault_prediction
+
+conda create -n wafer_fault python=3.8 -y
+conda activate wafer_fault
+
+pip install -r req.txt
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    author="Arkaprabha Banerjee",
+    author_email="arkaofficial13@gmail.com",
+    packages=find_packages(),
+    # install_requires="requirements.txt"
+    install_requires=[]
+    )
